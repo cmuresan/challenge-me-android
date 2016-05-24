cb477e7154c0c040a1e3198d7bf639cd7ab6a2b1
Cristian Muresan <muresan.cristi94@gmail.com>
2016-05-24 08:12:55 +0300

    GMaps

--- app/src/main/java/com/example/cristianmmuresan/challengeme/ui/record/RecordActivity.java
+++ app/src/main/java/com/example/cristianmmuresan/challengeme/ui/record/RecordActivity.java
@@ -1,21 +1,45 @@
 package com.example.cristianmmuresan.challengeme.ui.record;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.cristianmmuresan.challengeme.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class RecordActivity extends AppCompatActivity {
+
+    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
+            -73.998585);
+    private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
+    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        setUpMapIfNeeded();
+    }
 
+    private void setUpMapIfNeeded() {
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            if (googleMap != null) {
+                addLines();
+            }
+        }
+    }
+
+    private void addLines() {
+        googleMap.addPolyline((new PolylineOptions()).add(TIMES_SQUARE, BROOKLYN_BRIDGE,
+                LOWER_MANHATTAN, TIMES_SQUARE).width(4).color(getResources().getColor(R.color.colorAccent)).geodesic(true));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOWER_MANHATTAN, 13));
     }
 
 }
